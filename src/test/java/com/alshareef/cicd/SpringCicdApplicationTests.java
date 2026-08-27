9a9de27f9fd3f381e350da50690e7d2c3f555606
Shareef Alam <alshareef4039@gmail.com>
2026-08-27 15:50:46 +0530

    code init

---
+++ src/test/java/com/alshareef/cicd/SpringCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alshareef.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
